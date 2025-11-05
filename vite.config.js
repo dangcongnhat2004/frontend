4f9be105a10e540f1a3c9be72810b90fa7f9119a
Đặng Công Nhật <125759308+dangcongnhat2004@users.noreply.github.com>
2025-11-06 04:57:04 +0700

    Add base path for asset loadingupdate
    Set base path for asset loading on deployment.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",   // ✅ BẮT BUỘC để load đúng asset khi deploy
   plugins: [react()],
   server: {
-    host: true,  // để bạn truy cập IPv4
+    host: true,
     port: 5173,
   },
 });
